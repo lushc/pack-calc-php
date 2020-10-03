ceabaf9c0a90ad75c49e6408e129bf53aba6fec3
Chris Lush <chris.lush@gmail.com>
2020-10-03 20:14:20 +0100

    Add some basic benchmark stats to the CLI script

--- cli.php
+++ cli.php
@@ -4,6 +4,7 @@ use Graphp\GraphViz\GraphViz;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$startTime = microtime(true);
 $quantity = $argv[1];
 $packSizes = array_map('intval', explode(',', $argv[2]));
 
@@ -13,5 +14,10 @@ $packsRequired = $packCalc->calculate();
 foreach ($packsRequired as $size => $count) {
     print "{$count}x $size" . PHP_EOL;
 }
+
+$time = number_format(microtime(true) - $startTime, 2);
+$peakMemory = number_format(memory_get_peak_usage() / (1024 ** 2), 2);
+
+print PHP_EOL . "Generated in {$time} seconds with {$peakMemory} MB peak usage, now visualising the graph...";
 
 (new GraphViz())->display($packCalc->graph);
