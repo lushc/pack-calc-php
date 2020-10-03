68a39d12818b52661f8e9d8a837dd6ae30438897
Chris Lush <chris.lush@gmail.com>
2020-10-03 19:02:47 +0100

    Output the calculated pack sizes

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $quantity = $argv[1];
 $packSizes = [250, 500, 1000, 2000, 5000];
 $packCalc = new PackCalc($quantity, $packSizes);
+$packsRequired = $packCalc->calculate();
 
-$packCalc->calculate();
+foreach ($packsRequired as $size => $count) {
+    print "{$count}x $size" . PHP_EOL;
+}
+
 (new GraphViz())->display($packCalc->graph);
